b4417b15130147b05a07ee20da7b8e96171cc9b8
Tamas Dobo <github@dobota.com>
2018-12-25 20:43:47 +0000

    Add temp and door sensor

--- code/active/RPi-001/serialPublish.py
+++ code/active/RPi-001/serialPublish.py
@@ -1,22 +1,20 @@
 import serial;
 import paho.mqtt.client as mqtt
 import time
+import json
 
 broker_address="192.168.1.84" 
 client = mqtt.Client("RPi-1") #create new instance
 client.username_pw_set("mosquitto", password="pass");
 client.connect(broker_address) #connect to broker
 
-arduinoSerialData =serial.Serial('/dev/ttyACM1',9600)
+arduinoSerialData =serial.Serial('/dev/ttyACM0',9600)
 while 1:
 	if(arduinoSerialData.inWaiting()>0):
-		myData = int(arduinoSerialData.readline())
+            try:
+		myData = json.loads(arduinoSerialData.readline())
                 
-                if (myData == 17990):
-                    client.publish("home/test/led", 'OFF')
-                    print "Publishing: OFF"
-               
-                if (myData == 20302):
-                    client.publish("home/test/led", 'ON')
-                    print "Publishing: ON"
-
+                print json.dumps(myData)
+                client.publish("home/test/sensor/array/1", json.dumps(myData))
+            except ValueError:
+                print 'Try again...'
